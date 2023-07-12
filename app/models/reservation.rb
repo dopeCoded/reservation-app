f670536317cb4491941f932ab6c1d40bb79c3f08
tanaka <tanaka.for.public@gmail.com>
2023-07-12 22:10:55 +0900

    チェックイン日が過去の日付でないことを確認するバリデーションを追加

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -8,13 +8,22 @@ class Reservation < ApplicationRecord
   validates :check_out, presence: true
   validates :guests, presence: true
 
+  validate :check_in_date_cannot_be_in_the_past
+  validate :check_out_after_check_in
+
+  validates :guests, numericality: { greater_than_or_equal_to: 1, message: "At least one person" }
+
   def total_cost
     self.room.price * self.guests * ((self.check_out - self.check_in) / 86400).to_i
   end  
-  validates :check_in, :check_out, :guests, presence: true
-  validate :check_out_after_check_in
-  validates :guests, numericality: { greater_than_or_equal_to: 1, message: "At least one person" }
-  
+
+  private
+
+  def check_in_date_cannot_be_in_the_past
+    if check_in.present? && check_in < Date.today
+      errors.add(:check_in, "can't be in the past")
+    end
+  end
 
   def check_out_after_check_in
     return if check_out.blank? || check_in.blank?
